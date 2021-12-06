Fixed a bug in the computation of the log-signature

examples.py
@@ -157,14 +157,14 @@ def smooth_vf_smooth_path(n=100, N=2, k=4, plot=False, exact=False, n_steps=100,
     return solution, error_bound, toc - tic
 
 
-def smooth_vf_smooth_path_discussion(n_vec=np.array([100, 147, 215, 316, 464, 681, 1000, 1470, 2150]),
+def smooth_vf_smooth_path_discussion(n_vec=np.array([100, 215, 464, 1000, 2150]),
                                      N_vec=np.array([1, 2, 3]), k=4, exact=False,
-                                     n_steps_vec=np.array([1, 2, 3, 5, 7, 10, 15, 22, 32]), method='RK45',
+                                     n_steps_vec=np.array([1, 2, 3, 10, 32]), method='RK45',
                                      atol=1e-09, rtol=1e-06, h=1e-07, norm=lo.l1, p=1, var_steps=15, show=False,
                                      save=False,
                                      dir='C:/Users/breneis/Desktop/Backup 09112021/Studium/Mathematik WIAS/T/9999-99 Main file/LogODE plots',
                                      rounds=10, adaptive_tol=False):
-    kind = 'smooth vector field, smooth path'
+    kind = 'smooth vector field, smooth pathqq'
     solutions = np.zeros((len(N_vec), len(n_vec), len(n_steps_vec), 2))
     error_bounds = np.zeros((len(N_vec), len(n_vec), len(n_steps_vec)))
     times = np.zeros((len(N_vec), len(n_vec), len(n_steps_vec)))

logode.py
@@ -120,7 +120,7 @@ def log_sig(x, n):
     log_sig = []
     for k in range(1, n + 1):
         # computing the k-th level of the log-signature
-        ls = sig[k - 1]
+        ls = sig[k - 1].copy()
         for i in range(2, k + 1):
             # here are the terms of the k-th level we get from (-1)**(i+1) * 1/i * X**i
             ls_i = 0
@@ -128,10 +128,10 @@ def log_sig(x, n):
             for partition in partitions:
                 # we have a specific partition x^l_1 * x^l_2 * ... * x^l_i with l_1 + l_2 + ... + l_i = k
                 partition = partition - 1  # indices start at 0
-                partition_tensor = sig[partition[0]]
+                partition_tensor = sig[partition[0]].copy()
                 for j in range(2, i + 1):
                     # compute the tensor product x^l_1 * ... * x^l_j
-                    partition_tensor = np.tensordot(partition_tensor, sig[partition[j - 1]], axes=0)
+                    partition_tensor = np.tensordot(partition_tensor, sig[partition[j - 1]].copy(), axes=0)
                 ls_i += partition_tensor
             ls += (-1) ** (i + 1) / i * ls_i
         log_sig.append(ls)